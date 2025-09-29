1c8b9cf6ee89873f038b1d548eeb697923372d79
Heshan Asmadala <Heshanasmadala@gmail.com>
2025-09-30 00:00:05 +0530

    user credits implmented

--- server/controllers/UserController.js
+++ server/controllers/UserController.js
@@ -70,4 +70,23 @@ const clerkWebhooks = async(req, res) =>{
 
 }
 
-export {clerkWebhooks}
+
+
+
+
+// API controller function to get user available credits data
+
+const userCredits = async (req, res) =>{
+    try {
+
+        const userData = await userModel.findone({clerkId})
+        res.json({success:true, credits: userData.creditBalance})
+        
+    } catch (error) {
+        console.log(error.message);
+        res.json({success:false, message:error.message})
+    }
+}
+
+
+export {clerkWebhooks, userCredits}
