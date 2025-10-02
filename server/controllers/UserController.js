f76e6e5653f52856b8f2ea5fe8d4d3acddcc8d65
Heshan Asmadala <Heshanasmadala@gmail.com>
2025-10-02 11:19:19 +0530

    Stripe payment

--- server/controllers/UserController.js
+++ server/controllers/UserController.js
@@ -1,92 +1,149 @@
-import {Webhook} from 'svix'
-import userModel from '../models/userModel.js'
-import connectDB from '../configs/mongodb.js'
+import { Webhook } from "svix";
+import Stripe from "stripe";
+import userModel from "../models/userModel.js";
+import connectDB from "../configs/mongodb.js";
+import transactionModel from "../models/transactionModel.js";
 
-// API controller Function to manage Clerk user with database
-// http:localhost:400/api/user/webhooks
-const clerkWebhooks = async(req, res) =>{
+// Initialize Stripe
+const stripeInstance = new Stripe(process.env.STRIPE_SECRET_KEY);
 
-    try {
-        await connectDB();
+const clerkWebhooks = async (req, res) => {
+  try {
+    await connectDB();
 
-        const whook = new Webhook(process.env.CLERK_WEBHOOK_SECRET)
-        
-        await whook.verify(JSON.stringify(req.body),{
-            "svix-id":req.headers["svix-id"],
-            "svix-timestamp":req.headers["svix-timestamp"],
-            "svix-signature":req.headers["svix-signature"]
-        })
+    const whook = new Webhook(process.env.CLERK_WEBHOOK_SECRET);
 
-        const {data, type} = req.body
+    await whook.verify(req.rawBody, {
+      "svix-id": req.headers["svix-id"],
+      "svix-timestamp": req.headers["svix-timestamp"],
+      "svix-signature": req.headers["svix-signature"],
+    });
 
-        switch (type) {
-            case "user.created":{
+    const { data, type } = req.body;
 
-                const userData = {
-                    clerkId: data.id,
-                    email: data.email_addresses[0].email_address,
-                    firstName: data.first_name,
-                    lastName: data.last_name,
-                    photo: data.image_url
-                }
+    switch (type) {
+      case "user.created": {
+        const userData = {
+          clerkId: data.id,
+          email: data.email_addresses[0].email_address,
+          firstName: data.first_name,
+          lastName: data.last_name,
+          photo: data.image_url,
+          creditBalance: 0, // default
+        };
 
-                await userModel.create(userData)
-                res.json({})
+        await userModel.create(userData);
+        res.json({ success: true });
+        break;
+      }
 
-                break;
-            }
-            case "user.updated":{
+      case "user.updated": {
+        const userData = {
+          email: data.email_addresses[0].email_address,
+          firstName: data.first_name,
+          lastName: data.last_name,
+          photo: data.image_url,
+        };
 
-                const userData = {
-                    email: data.email_addresses[0].email_address,
-                    firstName: data.first_name,
-                    lastName: data.last_name,
-                    photo: data.image_url
-                }
+        await userModel.findOneAndUpdate({ clerkId: data.id }, userData);
+        res.json({ success: true });
+        break;
+      }
 
-                await userModel.findOneAndUpdate({clerkId:data.id}, userData)
-                res.json({})
+      case "user.deleted": {
+        await userModel.findOneAndDelete({ clerkId: data.id });
+        res.json({ success: true });
+        break;
+      }
 
-                break;
-            }
-            case "user.deleted":{
+      default:
+        res.json({ success: true, message: "Unhandled event type" });
+        break;
+    }
+  } catch (error) {
+    console.log(error.message);
+    res.json({ success: false, message: error.message });
+  }
+};
 
-                await userModel.findOneAndDelete({clerkId:data.id})
-                res.json({}) 
+// Get user credits
+const userCredits = async (req, res) => {
+  try {
+    const { clerkId } = req.body; // or req.query depending on frontend
 
-                break;
-            }   
-                
-        
-            default:
-                break;
-        }
+    const userData = await userModel.findOne({ clerkId });
 
-    } catch (error) {
-        console.log(error.message);
-        res.json({success:false, message:error.message})
-        
+    if (!userData) {
+      return res.json({ success: false, message: "User not found" });
     }
 
-}
+    res.json({ success: true, credits: userData.creditBalance });
+  } catch (error) {
+    console.log(error.message);
+    res.json({ success: false, message: error.message });
+  }
+};
 
+// Buy credits
+const buyCredit = async (req, res) => {
+  try {
+    const { clerkId, planId } = req.body;
 
+    await connectDB();
 
-
-
-// API controller function to get user available credits data
-
-const userCredits = async (req, res) =>{
-    try {
-
-        const userData = await userModel.findone({clerkId})
-        res.json({success:true, credits: userData.creditBalance})
-        
-    } catch (error) {
-        console.log(error.message);
-        res.json({success:false, message:error.message})
+    const userData = await userModel.findOne({ clerkId });
+    if (!userData || !planId) {
+      return res.json({ success: false, message: "Invalid credentials" });
     }
-}
 
+    let credits, plan, amount;
+    switch (planId) {
+      case "Basic":
+        plan = "Basic";
+        credits = 100;
+        amount = 10;
+        break;
+      case "Advanced":
+        plan = "Advanced";
+        credits = 500;
+        amount = 50;
+        break;
+      case "Business":
+        plan = "Business";
+        credits = 5000;
+        amount = 250;
+        break;
+      default:
+        return res.json({ success: false, message: "Invalid plan" });
+    }
 
-export {clerkWebhooks, userCredits}
+    // Create transaction record
+    const transactionData = {
+      clerkId,
+      plan,
+      amount,
+      credits,
+      payment: false,
+      date: Date.now(),
+    };
+    const newTransaction = await transactionModel.create(transactionData);
+
+    // Stripe PaymentIntent
+    const paymentIntent = await stripeInstance.paymentIntents.create({
+      amount: amount * 100,
+      currency: process.env.CURRENCY || "usd",
+      metadata: { transactionId: newTransaction._id.toString(), clerkId },
+    });
+
+    res.json({
+      success: true,
+      clientSecret: paymentIntent.client_secret,
+      transactionId: newTransaction._id,
+    });
+  } catch (error) {
+    console.log(error.message);
+    res.json({ success: false, message: error.message });
+  }
+};
+
+export { clerkWebhooks, userCredits, buyCredit };
